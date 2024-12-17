ae7602242035cfc74cd781d6f1008514547e4345
gimmingeon <mnbqwe123987@naver.com>
2024-12-17 09:50:57 +0900

    Feat: post 엔티티 추가 및 연결

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,32 @@
 import { Module } from '@nestjs/common';
-// import { AppController } from './app.controller';
-// import { AppService } from './app.service';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import { PostModule } from './post/post.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [PostModule],
-  controllers: [/*AppController*/],
-  providers: [/*AppService*/],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      // configModule을 사용하기 위해 configService를 사용
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('DATABASE_HOST'),
+        port: +configService.get('DATABASE_PORT'),
+        username: configService.get('DATABASE_USER'),
+        password: configService.get('DATABASE_PASSWORD'),
+        database: configService.get('DATABASE_NAME'),
+        entities: [],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+    PostModule
+  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule { }
--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,5 +1,6 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { PostService } from './post.service';
+import { CreatePostDto } from './dto/create-post.dto';
 
 @Controller('post')
 export class PostController {
@@ -8,5 +9,16 @@ export class PostController {
         private readonly postService: PostService
     ) { }
 
+    // @Post()
+    // async createPost(
+    //     @Body() createPostDto: CreatePostDto,
+    // ) {
 
+    // }
+
+    // 게시판 전체 조회
+    @Get('')
+    async findAllPost() {
+        return await this.postService.findAllPost();
+    }
 }
